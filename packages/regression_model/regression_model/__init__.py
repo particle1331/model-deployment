f3bd78d3938ace8cb8dd6d4a1c78eafa1e8af287
particle <54845098+particle1331@users.noreply.github.com>
2022-03-30 22:34:25 +0800

    Update init and black version.

--- packages/regression_model/regression_model/__init__.py
+++ packages/regression_model/regression_model/__init__.py
@@ -1,17 +1,7 @@
-import logging
+from regression_model.config.core import PACKAGE_ROOT
 
-from regression_model.config.core import PACKAGE_ROOT, config
+VERSION_PATH = PACKAGE_ROOT / "VERSION"
+name = "regression_model"
 
-# It is strongly advised that you do not add any handlers other than
-# NullHandler to your library’s loggers. This is because the configuration
-# of handlers is the prerogative of the application developer who uses your
-# library. The application developer knows their target audience and what
-# handlers are most appropriate for their application: if you add handlers
-# ‘under the hood’, you might well interfere with their ability to carry out
-# unit tests and deliver logs which suit their requirements.
-# https://docs.python.org/3/howto/logging.html#configuring-logging-for-a-library
-logging.getLogger(config.app_config.package_name).addHandler(logging.NullHandler())
-
-
-with open(PACKAGE_ROOT / "VERSION") as version_file:
+with open(VERSION_PATH, "r") as version_file:
     __version__ = version_file.read().strip()
