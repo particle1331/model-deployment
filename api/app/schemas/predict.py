aabcf87c17c90d9881ad2fc4b140e50ad0fb8bb4
particle <54845098+particle1331@users.noreply.github.com>
2022-04-14 02:02:58 +0800

    Update ff. regression_model version 0.1.0

--- api/app/schemas/predict.py
+++ api/app/schemas/predict.py
@@ -1,7 +1,7 @@
 from typing import Any, List, Optional
 
 from pydantic import BaseModel
-from regression_model.processing.validation import HouseDataInputSchema
+from regression_model.processing.schemas import HouseDataInputSchema
 
 
 class PredictionResults(BaseModel):
