fb66145fd6b94af6f6735f2ce1f4f9203ebea281
particle <54845098+particle1331@users.noreply.github.com>
2022-05-18 03:33:24 +0800

    Fix syntax error in publish script.

--- packages/regression_model/publish_model.py
+++ packages/regression_model/publish_model.py
@@ -4,6 +4,6 @@ import glob
 
 for p in glob.glob('dist/*.whl'):
     try:
-        os.system(f'curl -F package=@{p} {os.environ['GEMFURY_PUSH_URL']}')
+        os.system(f'curl -F package=@{p} {os.environ["GEMFURY_PUSH_URL"]}')
     except:
         raise Exception("Uploading package failed on file {p}")
