921ac6147749069546a144c1565cde94d503c18d
particle <54845098+particle1331@users.noreply.github.com>
2022-05-17 22:33:00 +0800

    Typo with GEMFURY_PUSH_URL environment variable.

--- packages/regression_model/publish_model.py
+++ packages/regression_model/publish_model.py
@@ -2,10 +2,10 @@ import os
 import glob
 
 
-GEMFURY_URL = os.environ['GEMFURY_URL']
+GEMFURY_PUSH_URL = os.environ['GEMFURY_PUSH_URL']
 
 for p in glob.glob('dist/*.whl'):
     try:
-        os.system(f'curl -F package=@{p} {GEMFURY_URL}')
+        os.system(f'curl -F package=@{p} {GEMFURY_PUSH_URL}')
     except:
         raise Exception("Uploading package failed on file dist/{p}")
