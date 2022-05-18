7765b3f0493c3acac5bb0156b5f9723b6713693c
particle <54845098+particle1331@users.noreply.github.com>
2022-05-18 14:56:30 +0800

    Remove CircleCI API token.

--- packages/regression_model/publish_model.py
+++ packages/regression_model/publish_model.py
@@ -7,17 +7,3 @@ for p in glob.glob('dist/*.whl'):
         os.system(f'curl -F package=@{p} {os.environ["GEMFURY_PUSH_URL"]}')
     except:
         raise Exception("Uploading package failed on file {p}")
-
-# CircleCI API trigger
-request = f"""curl --request POST \
-  'https://circleci.com/api/v2/project/github/particle1331/model-deployment/pipeline' \
-  -H f'Circle-Token: {os.environ["CIRCLE_TOKEN"]}' \
-  -H 'Content-Type: application/json' \
-  -d '{{
-    "parameters": {{
-      "run_regression_model": false,
-      "run_deploy_app": true
-    }}
-}}'
-"""
-os.system(request)
