3bde16759f62727c92d35719461629c604ce09be
particle <54845098+particle1331@users.noreply.github.com>
2022-05-18 13:08:12 +0800

    Test CircleCI API trigger.

--- packages/regression_model/publish_model.py
+++ packages/regression_model/publish_model.py
@@ -7,3 +7,10 @@ for p in glob.glob('dist/*.whl'):
         os.system(f'curl -F package=@{p} {os.environ["GEMFURY_PUSH_URL"]}')
     except:
         raise Exception("Uploading package failed on file {p}")
+
+os.system(f"""curl --request POST \
+	--url https://circleci.com/api/v2/project/vcs-slug/org-name/repo-name/pipeline \
+	--header 'Circle-Token: {os.environ["CIRCLE_TOKEN"]}' \
+	--header 'content-type: application/json' \
+	--data '{"parameters":{"run_regression_model":false, "run_deploy_app":true}}'
+""")
